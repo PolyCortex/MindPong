648fd8e6f944700fbff304af6b8f2b304d2839b0
William Harvey <william.harvey@polymtl.ca>
2018-09-12 00:33:45 -0400

    CO-WH: setup inserted in a try except block + pyMuse installation works

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ if (system() != 'Windows'):
 
 pyqt_link = ''
 if machine() == 'AMD64':
-    pyqt_link = 'https://download.lfd.uci.edu/pythonlibs/o4uhg4xd/PyQt4-4.11.4-cp27-cp27m-win_amd64.whl'
+    pyqt_link = 'https://download.lfd.uci.edu/pythonlibs/o4uhg4xd/PyQt4-4.11.4-cp27-cp27m-win_amd64.whl#egg=PyQt4'
 elif machine() == 'x86':
-    pyqt_link = 'https://download.lfd.uci.edu/pythonlibs/o4uhg4xd/PyQt4-4.11.4-cp27-cp27m-win32.whl'
+    pyqt_link = 'https://download.lfd.uci.edu/pythonlibs/o4uhg4xd/PyQt4-4.11.4-cp27-cp27m-win32.whl#egg=PyQt4'
 else:
     sys.exit('Your architecture is not compatible with any version of PyQt4. Sorry :(')
 
@@ -21,33 +21,37 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(
-    name='MindPong',
-    version='0.1',
+try:
+    setup(
+        name='MindPong',
+        version='0.1',
 
-    description='MindPong is a fun game demonstrating neurotechnologies',
-    long_description=long_description,
-    url='https://github.com/PolyCortex/MindPong',
-    author='PolyCortex',
-    author_email='polycortex@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Application',
-        'Programming Language :: Python :: 2.7',
-    ],
-    keywords='muse polycortex eeg concentration beta',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    install_requires=['pexpect', 'pyserial', 'numpy'],
-    dependency_links=[pyqt_link, 'https://github.com/PolyCortex/pyMuse/archive/ExtractMindPongApp.zip'],
-
-    entry_points={
-        'console_scripts': [
-            'boatGUI=boatGUI:main',
+        description='MindPong is a fun game demonstrating neurotechnologies',
+        long_description=long_description,
+        url='https://github.com/PolyCortex/MindPong',
+        author='PolyCortex',
+        author_email='polycortex@gmail.com',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Application',
+            'Programming Language :: Python :: 2.7',
         ],
-    },
-)
+        keywords='muse polycortex eeg concentration beta',
+        packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-print('\nINSTALLATION COMPLETED!')
+        install_requires=['pexpect', 'pyserial', 'numpy', 'wheel', 'pyMuse', ' PyQt4'],
+        dependency_links=[ 'https://github.com/PolyCortex/pyMuse/archive/ExtractMindPongApp.zip#egg=pyMuse'],
+
+        entry_points={
+            'console_scripts': [
+                'boatGUI=boatGUI:main',
+            ],
+        },
+    )
+    print('\nINSTALLATION COMPLETED!')
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
+
