a7b92f4c078704862e632af13b62a0b8e5c030ab
conorato <c.onorato@hotmail.com>
2018-10-09 16:58:52 -0400

    Extract constants to constants file

---
+++ constants.py
@@ -0,0 +1,15 @@
+BACKGROUND_COLORS = {
+    "RED": "background-color: red",
+    "GREEN": "background-color: red",
+    "GREY": "background-color: red"
+}
+
+WINDOW_SIZE = {
+    "W": 1300,
+    "H": 700
+}
+
+BUTTON_SIZE = {
+    "W": 100,
+    "H": 30
+}
