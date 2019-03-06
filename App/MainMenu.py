ace1fc10b27a360c75f83d184455e07b489dc922
Monssaf Toukal <tmonssaf@gmail.com>
2019-03-05 21:30:09 -0500

    refactor minimal de la classe MainMenu pour faciliter l'integration des tabs

--- App/MainMenu.py
+++ App/MainMenu.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt5 tutorial
-
-In this example, we create a simple
-window in PyQt5.
-
-Author: Jan Bodnar
-Website: zetcode.com
-Last edited: August 2017
-"""
-
-import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QTabBar
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow
 from PyQt5.QtGui import QIcon
+import sys
 
 
-class MainMenu(QWidget):
-    currentDirectory = os.path.dirname(os.path.realpath(__file__))
-    logoPath = currentDirectory + os.path.sep + 'Images' + os.path.sep + 'logo_polyCortex.png'
+class MainMenu(QMainWindow):
+    Y_COORD_UPPER_BOUND = 1080  # random value based on my screen dimensions
+    X_COORD_UPPER_BOUND = 1920  # random value based on my screen dimensions
+    DEFAULT_MENU_HEIGHT = 800
+    DEFAULT_MENU_WIDTH = 640
+
     def __init__(self):
         super().__init__()
-        self.width = 1920
-        self.height = 1080
-        self.x = 0
-        self.y = 100
+        self._currentDirectory = os.path.dirname(os.path.realpath(__file__))
+        self._logoPath = self._currentDirectory + os.path.sep + 'Images' + os.path.sep + 'logo_polyCortex.png'
+        self._menuWidth = 800
+        self._menuHeight = 640
         self.init_ui()
 
+    @property
+    def current_directory(self):
+        return self._currentDirectory
+
+    @property
+    def logo_path(self):
+        return self._logoPath
+
+    def center_menu(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2,
+                  (screen.height() - size.height()) / 2)
+
     def init_ui(self):
-
-        print(self.currentDirectory)
-        self.setGeometry(self.x, self.y, self.width, self.height)
+        self.center_menu()
+        self.resize(MainMenu.DEFAULT_MENU_HEIGHT, MainMenu.DEFAULT_MENU_WIDTH)
         self.setWindowTitle('MindPong')
-        self.setWindowIcon(QIcon(self.logoPath))
-
+        self.setWindowIcon(QIcon(self._logoPath))
         self.show()
-
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainMenu()
+    menu = MainMenu()
     sys.exit(app.exec_())
