e287c2bc3702d6da8af4f1c9d9e4a098a59a882d
conorato <c.onorato@hotmail.com>
2019-04-01 12:49:52 -0400

    switched update_data to game model class

--- mindpong/model/game.py
+++ mindpong/model/game.py
@@ -1,6 +1,10 @@
+from threading import Thread
+import sys
+
+import numpy as np
 from enum import Enum
 
-from mindpong.model.player import Player
+from mindpong.model.player import Player, SIGNAL_NAMES
 
 DEFAULT_PORT_PLAYER_ONE = 5001
 DEFAULT_PORT_PLAYER_TWO = 5002
@@ -15,15 +19,40 @@ class Game():
     def __init__(self, signals_callback):
         self.state: GameState = GameState.INITIAL
         self.winner = None
+        self.callbacks = signals_callback
         self.players = [
             Player(DEFAULT_PORT_PLAYER_ONE, signals_callback),
             Player(DEFAULT_PORT_PLAYER_TWO, signals_callback)
         ]
 
     def start(self):
+        if self.state is GameState.INITIAL:
+            for player in self.players:
+                player.start()
+                player.is_playing = True
+
+            self._update_thread = Thread(target=self._update_signal).start()
+            self.state = GameState.IN_PLAY
+
+    def _update_signal(self):
+        print("Game started with "+ str(len(self.players)) + " players")
+        while(self.state is GameState.IN_PLAY):
+            try:
+                data = [self._get_mean_signal(player) for player in self.players]
+            except SystemExit:
+                self.state = GameState.INITIAL
+                break
+                
+            for callback in self.callbacks:
+                callback(data)
+
         for player in self.players:
-            player.start()
-        self.state = GameState.IN_PLAY
+            player.stop()
+        sys.exit()
+
+    def _get_mean_signal(self, player):
+        data: SignalData = player.signals.read(SIGNAL_NAMES[0])
+        return (data.time, np.nanmean(data.values))
 
     
 
--- mindpong/model/player.py
+++ mindpong/model/player.py
@@ -1,10 +1,5 @@
-from threading import Thread
-
-import numpy as np
-
 from pymuse.inputstream.muse_osc_input_stream import MuseOSCInputStream
 from pymuse.signal import SignalData
-from pymuse.configureshutdown import configure_shutdown
 
 DEFAULT_IP_ADDRESS = "127.0.0.1"
 SIGNAL_NAMES = ['beta_relative']
@@ -19,8 +14,6 @@ class Player(object):
         self._signal_callbacks = signal_callbacks
         self.is_playing = False
         self.signals = MuseOSCInputStream(SIGNAL_NAMES, ip_address, port)
-        configure_shutdown(self.signals)
-
 
     @property
     def ip_address(self):
@@ -52,25 +45,9 @@ class Player(object):
 
     def start(self):
         if not self.is_playing:
-            try:
-                self.signals.start()
-                self._update_thread = Thread(target=self._update_signal).start()
-            except Exception as e:
-                print(e)
-                raise Exception("Player: Unable to start thread")
+            self.signals.start()
             self.is_playing = True
 
     def stop(self):
-        if self.is_playing:
-            self.is_playing = False
-            self._update_thread.join()
-            self.signals.shutdown()
-
-    def _update_signal(self):
-        signals = []
-        print("Player on port " + str(self._port) + ": started acquisition")
-        while(self.is_playing):
-            data: SignalData = self.signals.read(SIGNAL_NAMES[0])
-            signal = (data.time, np.nanmean(data.values))
-            for callback in self._signal_callbacks:
-                callback(signal)
+        self.is_playing = False
+        self.signals.shutdown()
