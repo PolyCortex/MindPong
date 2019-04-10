bf25aef9dfb7504df0ad70ab359d085758eea576
conorato <c.onorato@hotmail.com>
2019-04-09 20:37:05 -0400

    fixed placement labels

--- mindpong/view/PlayTab.py
+++ mindpong/view/PlayTab.py
@@ -1,34 +1,41 @@
 import os
 
-from PyQt5.QtGui import (QFont, QPixmap)
+from PyQt5.QtGui import (QFont, QPixmap, QTransform)
 from PyQt5.QtWidgets import (
     QTabWidget, QGridLayout, QLabel, 
     QPushButton, QDialog, QMessageBox
 )
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 import emoji
 from serial import SerialException
 
 
-from mindpong.utils import get_project_root
-from mindpong.view.utils import BACKGROUND_COLORS, IMAGES_PATH, MINDPONG_TITLE
+from mindpong.view.utils import (
+    BACKGROUND_COLORS, IMAGES_PATH, MINDPONG_TITLE,
+    get_image_file)
 from mindpong.model.game import GameState
 
 ARROW_FILE_NAME = 'arrow.png'
 PINGPONG_FILE_NAME = 'ball.png'
+RED_PLAYER_FILE_NAME = 'red_player.png'
+BLUE_PLAYER_FILE_NAME = 'blue_player.png'
+
 
 class PlayTab(QTabWidget):
+    update_signal_event = pyqtSignal()
 
     START_GAME_STRING = "▶️ Start"
     STOP_GAME_STRING = "⏹️ Stop"
 
-    ARROW_SCALE = (1, 1)
-    BALL_SCALE = (1/3, 1/3)
+    # (width_scale, height_scale)
+    ARROW_SCALES = [(0.75, 0.75), (0.75, 0.75)] 
+    BALL_SCALE = (0.25, 0.25)
 
     def __init__(self):
         super().__init__()
-        self.arrowPath = os.path.sep.join([get_project_root(), IMAGES_PATH, ARROW_FILE_NAME])
-        self.ballPath = os.path.sep.join([get_project_root(), IMAGES_PATH, PINGPONG_FILE_NAME])
+        self.arrowPath = get_image_file(ARROW_FILE_NAME)
+        self.ballPath = get_image_file(PINGPONG_FILE_NAME)
+        self.playersPath = [get_image_file(RED_PLAYER_FILE_NAME), get_image_file(BLUE_PLAYER_FILE_NAME)]
 
         self.centralGridLayout: QGridLayout
         self.playButton = QPushButton(emoji.emojize(PlayTab.START_GAME_STRING))
@@ -37,53 +44,95 @@ class PlayTab(QTabWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.set_labels_layout()
+        self.init_labels_layout()
         self.init_buttons()
         self.init_error_message_box()
+        self.init_callbacks()
 
-    def set_labels_layout(self):
+    def init_labels_layout(self):
         self.centralGridLayout = QGridLayout()
         self.setLayout(self.centralGridLayout)
+        # Create Text Labels
         self.set_players_labels()
-        self.arrow_label = self.set_picture_label(self.arrowPath, ((0, 3, 1, 1)), self.ARROW_SCALE)
-        self.ball_label = self.set_picture_label(self.ballPath, (0, 2, 1, 1), self.BALL_SCALE)
         self.centralGridLayout.addWidget(QLabel("Math Question: "), 1, 0, 1, 2)
-        self.centralGridLayout.addWidget(self.playButton, 2, 1, 1, 3)
+
+        # Create Button Labels
+        self.centralGridLayout.addWidget(self.playButton, 2, 2, 1, 3)
+
+        # Create Pixmap Labels
+        self.arrow_labels = []
+        self.arrow_labels.append(self.get_picture_label(self.arrowPath, self.ARROW_SCALES[0], (Qt.AlignLeft | Qt.AlignVCenter))) 
+        self.centralGridLayout.addWidget(self.arrow_labels[0], 0, 4, 1, 1)
+
+        self.arrow_labels.append(self.get_picture_label(self.arrowPath, self.ARROW_SCALES[1], (Qt.AlignRight | Qt.AlignVCenter))) 
+        self.centralGridLayout.addWidget(self.mirror_player_arrow(self.arrow_labels[1]), 0, 2, 1, 1)
+
+        self.ball_label = self.get_picture_label(self.ballPath, self.BALL_SCALE, Qt.AlignCenter)
+        self.centralGridLayout.addWidget(self.ball_label, 0, 3, 1, 1)
+        self.ball_label.setFixedWidth(self.ball_label.pixmap().width())
+
 
     def set_players_labels(self):
         players = [QLabel("Player one"), QLabel("Player two")]
         for player in players:
-            player.setFont(QFont("Times", 16, QFont.Bold))
+            player.setFont(QFont("Times", 18, QFont.Bold))
             player.setMargin(30)
-            player.setAlignment(Qt.AlignCenter)
-        self.centralGridLayout.addWidget(players[0], 0, 0)
-        self.centralGridLayout.addWidget(players[1], 0, 6)
+            player.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        self.centralGridLayout.addWidget(players[0], 0, 0, 1, 2)
+        self.centralGridLayout.addWidget(players[1], 0, 5, 1, 2)
 
-    def set_picture_label(self, path, positions, scale):
-        label = QLabel()
-        label.setAlignment(Qt.AlignCenter)
-        pixmap = QPixmap(path)
+    def mirror_player_arrow(self, label):
+        label.setPixmap(label.pixmap().transformed(QTransform().scale(-1,1)))
+        return label
         
-        label.setPixmap(pixmap.scaled(pixmap.width() * scale[0], pixmap.height() * scale[1], Qt.KeepAspectRatio))
-        label.setFixedWidth(pixmap.width() * scale[0])
-        self.centralGridLayout.addWidget(label, positions[0], positions[1], positions[2], positions[3])
+
+    def get_picture_label(self, path, scale, alignment):
+        label = QLabel()
+        label.setAlignment(alignment)
+        label.setPixmap(QPixmap(path))
+        self._update_pixmap_scale(label, scale)        
         return label
 
     def init_buttons(self):
         self.playButton.setStyleSheet(BACKGROUND_COLORS['GREEN'])
-        self.playButton.clicked.connect(self.click_start_button_callback)
+        self.playButton.clicked.connect(self._click_start_button_callback)
 
     def init_error_message_box(self):
         self.errorBox: QMessageBox = QMessageBox()
         self.errorBox.setIcon(QMessageBox.Warning)
         self.errorBox.setWindowTitle(MINDPONG_TITLE)
 
+    def init_callbacks(self):
+        self.update_signal_event.connect(self._update_signal)
+
+    ###########################################################
+
     def set_delegate(self, delegate):
         self.delegate = delegate
 
-    def click_start_button_callback(self):
+    def _update_pixmap_scale(self, label, scale):
+        pixmap: QPixmap = label.pixmap()
+        label.setPixmap(pixmap.scaled(pixmap.width() * scale[0], pixmap.height() * scale[1], Qt.KeepAspectRatio))
+
+    def _update_signal(self, signal):
+        # player 1 signal means - player 2 signal means
+        signal_difference = signal[0][1] - signal[1][1]
+
+        if signal_difference > 0: # player 1 is winning
+            pass
+        elif signal_difference < 0: # player 2 is winning
+            pass
+        else:
+            pass
+
+        #self.ARROW_SCALE = ()
+        #_update_pixmap_scale(self.arrow_label, self.ARROW_SCALE)
+
+
+    def _click_start_button_callback(self):
+
         if self.delegate and self.delegate.game.state == GameState.INITIAL:
-            self.start_game()
+            self._start_game()
 
         elif self.delegate and self.delegate.game.state == GameState.IN_PLAY:
             self.delegate.end_game()
@@ -93,14 +142,9 @@ class PlayTab(QTabWidget):
         else:
             print("error in game state \n")
 
-    def resizeEvent(self, event):
-        # TODO: adjust labels to fit the screen correctly
-        # https://www.riverbankcomputing.com/static/Docs/PyQt4/qresizeevent.html
-        return super(PlayTab, self).resizeEvent(event)
-
-    def start_game(self):
+    def _start_game(self):
         try:
-            self.delegate.start_game()
+            self.delegate._start_game()
         except SerialException as e:
             self.errorBox.setText("Error: can't connect to serial %s port" % (self.delegate.serial_communication.port))
             self.errorBox.show()
@@ -108,3 +152,8 @@ class PlayTab(QTabWidget):
 
         self.playButton.setText(PlayTab.STOP_GAME_STRING)
         self.playButton.setStyleSheet(BACKGROUND_COLORS["RED"])
+
+    def resizeEvent(self, event):
+        # TODO: adjust labels to fit the screen correctly
+        # https://www.riverbankcomputing.com/static/Docs/PyQt4/qresizeevent.html
+        return super(PlayTab, self).resizeEvent(event)
