71835f734bdf23d49458c5b8fdf55d89ce136022
conorato <c.onorato@hotmail.com>
2019-04-09 20:52:06 -0400

    extracted set pixmap labels

--- mindpong/view/PlayTab.py
+++ mindpong/view/PlayTab.py
@@ -33,14 +33,11 @@ class PlayTab(QTabWidget):
 
     def __init__(self):
         super().__init__()
-        self.arrowPath = get_image_file(ARROW_FILE_NAME)
-        self.ballPath = get_image_file(PINGPONG_FILE_NAME)
         self.playersPath = [get_image_file(RED_PLAYER_FILE_NAME), get_image_file(BLUE_PLAYER_FILE_NAME)]
 
         self.centralGridLayout: QGridLayout
         self.playButton = QPushButton(emoji.emojize(PlayTab.START_GAME_STRING))
         self.countDownModal = QDialog(self)
-        self.gameState = 0
         self.init_ui()
 
     def init_ui(self):
@@ -52,24 +49,14 @@ class PlayTab(QTabWidget):
     def init_labels_layout(self):
         self.centralGridLayout = QGridLayout()
         self.setLayout(self.centralGridLayout)
+
         # Create Text Labels
         self.set_players_labels()
         self.centralGridLayout.addWidget(QLabel("Math Question: "), 1, 0, 1, 2)
-
         # Create Button Labels
         self.centralGridLayout.addWidget(self.playButton, 2, 2, 1, 3)
-
         # Create Pixmap Labels
-        self.arrow_labels = []
-        self.arrow_labels.append(self.get_picture_label(self.arrowPath, self.ARROW_SCALES[0], (Qt.AlignLeft | Qt.AlignVCenter))) 
-        self.centralGridLayout.addWidget(self.arrow_labels[0], 0, 4, 1, 1)
-
-        self.arrow_labels.append(self.get_picture_label(self.arrowPath, self.ARROW_SCALES[1], (Qt.AlignRight | Qt.AlignVCenter))) 
-        self.centralGridLayout.addWidget(self.mirror_player_arrow(self.arrow_labels[1]), 0, 2, 1, 1)
-
-        self.ball_label = self.get_picture_label(self.ballPath, self.BALL_SCALE, Qt.AlignCenter)
-        self.centralGridLayout.addWidget(self.ball_label, 0, 3, 1, 1)
-        self.ball_label.setFixedWidth(self.ball_label.pixmap().width())
+        self.set_pixmap_labels()
 
 
     def set_players_labels(self):
@@ -81,10 +68,21 @@ class PlayTab(QTabWidget):
         self.centralGridLayout.addWidget(players[0], 0, 0, 1, 2)
         self.centralGridLayout.addWidget(players[1], 0, 5, 1, 2)
 
-    def mirror_player_arrow(self, label):
-        label.setPixmap(label.pixmap().transformed(QTransform().scale(-1,1)))
-        return label
-        
+    def set_pixmap_labels(self):
+        # arrow pixmaps
+        self.arrow_labels = []
+        arrow_player_one = self.get_picture_label(get_image_file(ARROW_FILE_NAME), self.ARROW_SCALES[0], (Qt.AlignLeft | Qt.AlignVCenter))
+        self.arrow_labels.append(arrow_player_one) 
+        self.centralGridLayout.addWidget(self.arrow_labels[0], 0, 4, 1, 1)
+
+        arrow_player_two = self.get_picture_label(get_image_file(ARROW_FILE_NAME), self.ARROW_SCALES[1], (Qt.AlignRight | Qt.AlignVCenter))
+        self.arrow_labels.append(arrow_player_two) 
+        self.centralGridLayout.addWidget(self.mirror_player_arrow(self.arrow_labels[1]), 0, 2, 1, 1)
+
+        # ball pixmaps
+        self.ball_label = self.get_picture_label(get_image_file(PINGPONG_FILE_NAME), self.BALL_SCALE, Qt.AlignCenter)
+        self.centralGridLayout.addWidget(self.ball_label, 0, 3, 1, 1)
+        self.ball_label.setFixedWidth(self.ball_label.pixmap().width())
 
     def get_picture_label(self, path, scale, alignment):
         label = QLabel()
@@ -93,6 +91,10 @@ class PlayTab(QTabWidget):
         self._update_pixmap_scale(label, scale)        
         return label
 
+    def mirror_player_arrow(self, label):
+        label.setPixmap(label.pixmap().transformed(QTransform().scale(-1,1)))
+        return label
+        
     def init_buttons(self):
         self.playButton.setStyleSheet(BACKGROUND_COLORS['GREEN'])
         self.playButton.clicked.connect(self._click_start_button_callback)
