7f1661e50e5a1d71aa8cdd1393cceaa67bbd7c8d
conorato <c.onorato@hotmail.com>
2019-04-12 12:04:35 -0400

    added margin

--- mindpong/view/widgets/mathquestions.py
+++ mindpong/view/widgets/mathquestions.py
@@ -1,16 +1,17 @@
-from os import path
-
 from PyQt5.QtGui import QPixmap, QPainter, QTransform, QBrush, QColor
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import Qt, QSize, QRect
 
-STYLE_SHEET_PATH = path.join(path.dirname(__file__), "style.css")
+
 
 class MathQuestions(QWidget):
 
+    BOTTOM_MARGIN = 60
+    BACKGROUND_COLOR_CODE = '#cfdef7'
+
     def __init__(self):
         super().__init__()
-        self.setFixedHeight(self.height())
+        self.setFixedHeight(self.height() - self.BOTTOM_MARGIN)
 
     def sizeHint(self):
         return QSize(self.width(), self.height())
@@ -19,7 +20,10 @@ class MathQuestions(QWidget):
         painter = QPainter()
         painter.begin(self)
         
-        painter.setBrush(QBrush(QColor(207, 222, 247), Qt.Dense2Pattern))
+        color = QColor()
+        color.setNamedColor(self.BACKGROUND_COLOR_CODE)
+        painter.setBrush(QBrush(color, Qt.Dense2Pattern))
+        painter.setPen(Qt.darkBlue);
         painter.drawRoundedRect(0, 5, self.width()-5, self.height()-7, 3, 3);
 
         painter.end()
