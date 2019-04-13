d9227200f0e60e33c910056cb7f8efca6697b776
conorato <c.onorato@hotmail.com>
2019-04-12 20:57:48 -0400

    reordered methods play tab

--- mindpong/view/PlayTab.py
+++ mindpong/view/PlayTab.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtWidgets import (
@@ -35,52 +36,46 @@ class PlayTab(QTabWidget):
     def __init__(self):
         super().__init__()
         self.setStyle(QStyleFactory.create('Fusion'))
-        self.playersPath = [get_image_file(RED_PLAYER_FILE_NAME), get_image_file(BLUE_PLAYER_FILE_NAME)]
 
+        # Layout
         self.centralGridLayout: QGridLayout
-        self.playButton = QPushButton(emoji.emojize(PlayTab.START_GAME_STRING))
-        self.countDownModal = QDialog(self)
+        # Widgets
+        self.playButton: QPushButton
+        self.errorBox: QMessageBox 
+        # Custom Widgets
+        self.math_question_widget: MathQuestions
+        self.arrow_labels: List[ScalableArrow]
+        # Labels
+        self.player_logo_labels: List[QLabel]
+        self.player_title_labels: List[QLabel]
+        self.ball_label: QLabel
+
         self.init_ui()
 
-        self.counter = 0.75
 
     def init_ui(self):
-        self.init_labels_layout()
-        self.init_buttons()
-        self.init_error_message_box()
-        self.init_callbacks()
-
-    def init_labels_layout(self):
         self.centralGridLayout = QGridLayout()
         self.centralGridLayout.setRowMinimumHeight(2, 150)
         self.setLayout(self.centralGridLayout)
 
-        # Create Text Labels
-        self.set_players_labels()
-        # Create Button Labels
+        self.init_buttons()
+        self.init_error_message_box()
+        self.init_custom_widgets()
+        self.init_labels()
+        self.init_callbacks()
+
+    def init_buttons(self):
+        self.playButton = QPushButton(emoji.emojize(PlayTab.START_GAME_STRING))
+        self.playButton.setStyleSheet(BACKGROUND_COLORS['GREEN'])
+        self.playButton.clicked.connect(self._click_start_button_callback)
         self.centralGridLayout.addWidget(self.playButton, 2, 2, 1, 3)
-        # Create Pixmap Labels
-        self.set_pixmap_labels()
 
+    def init_error_message_box(self):
+        self.errorBox = QMessageBox()
+        self.errorBox.setIcon(QMessageBox.Warning)
+        self.errorBox.setWindowTitle(MINDPONG_TITLE)
 
-    def set_players_labels(self):
-        players = [QLabel("Player one"), QLabel("Player two")]
-        for player in players:
-            player.setFont(QFont("Times", 18, QFont.Bold))
-            player.setMargin(70)
-            player.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
-        self.centralGridLayout.addWidget(players[0], 0, 0, 1, 2)
-        self.centralGridLayout.addWidget(players[1], 0, 5, 1, 2)
-
-    def set_pixmap_labels(self):
-        # player logos
-        self.player_logo_labels = [
-            self.get_picture_label(get_image_file(RED_PLAYER_FILE_NAME), (0.25, 0.25), (Qt.AlignCenter)),
-            self.get_picture_label(get_image_file(BLUE_PLAYER_FILE_NAME), (0.25, 0.25), (Qt.AlignCenter))
-        ]
-        self.centralGridLayout.addWidget(self.player_logo_labels[0], 0, 0, 1, 2)
-        self.centralGridLayout.addWidget(self.player_logo_labels[1], 0, 5, 1, 2)
-
+    def init_custom_widgets(self):
         # arrow pixmaps
         self.arrow_labels = [ScalableArrow(is_mirrored=True), ScalableArrow()]
         self.centralGridLayout.addWidget(self.arrow_labels[0], 0, 2, 1, 1, (Qt.AlignVCenter))
@@ -90,10 +85,38 @@ class PlayTab(QTabWidget):
         self.math_question_widget = MathQuestions()
         self.centralGridLayout.addWidget(self.math_question_widget, 1, 1, 1, 5, (Qt.AlignVCenter))
 
-        # ball pixmaps
+    def init_labels(self):
+        # Create Player logos
+        self.player_logo_labels = [
+            self.get_picture_label(get_image_file(RED_PLAYER_FILE_NAME), (0.25, 0.25), (Qt.AlignCenter)),
+            self.get_picture_label(get_image_file(BLUE_PLAYER_FILE_NAME), (0.25, 0.25), (Qt.AlignCenter))
+        ]
+        self.centralGridLayout.addWidget(self.player_logo_labels[0], 0, 0, 1, 2)
+        self.centralGridLayout.addWidget(self.player_logo_labels[1], 0, 5, 1, 2)
+
+        # Create Player title labels
+        self.player_title_labels = self.set_players_labels()
+        self.centralGridLayout.addWidget(self.player_title_labels[0], 0, 0, 1, 2)
+        self.centralGridLayout.addWidget(self.player_title_labels[1], 0, 5, 1, 2)
+
+        # Create ball pixmaps
         self.ball_label = self.get_picture_label(get_image_file(PINGPONG_FILE_NAME), self.BALL_SCALE, Qt.AlignCenter)
         self.centralGridLayout.addWidget(self.ball_label, 0, 3, 1, 1)
         self.ball_label.setFixedWidth(self.ball_label.pixmap().width())
+
+
+    def set_players_labels(self):
+        players = [QLabel("Player one"), QLabel("Player two")]
+        for player in players:
+            player.setFont(QFont("Times", 18, QFont.Bold))
+            player.setMargin(70)
+            player.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        return players
+        
+    def init_callbacks(self):
+        self.update_signal_event.connect(self._update_signal)
+
+    ###########################################################
 
     def get_picture_label(self, path, scale, alignment):
         label = QLabel()
@@ -101,20 +124,6 @@ class PlayTab(QTabWidget):
         pixmap = QPixmap(path)
         label.setPixmap(pixmap.scaled(pixmap.width() * scale[0], pixmap.height() * scale[1]))
         return label
-        
-    def init_buttons(self):
-        self.playButton.setStyleSheet(BACKGROUND_COLORS['GREEN'])
-        self.playButton.clicked.connect(self._click_start_button_callback)
-
-    def init_error_message_box(self):
-        self.errorBox: QMessageBox = QMessageBox()
-        self.errorBox.setIcon(QMessageBox.Warning)
-        self.errorBox.setWindowTitle(MINDPONG_TITLE)
-
-    def init_callbacks(self):
-        self.update_signal_event.connect(self._update_signal)
-
-    ###########################################################
 
     def set_delegate(self, delegate):
         self.delegate = delegate
@@ -155,8 +164,7 @@ class PlayTab(QTabWidget):
 
         self.playButton.setText(PlayTab.STOP_GAME_STRING)
         self.playButton.setStyleSheet(BACKGROUND_COLORS["RED"])
-
-
+        
 
     def resizeEvent(self, event):
         # TODO: adjust labels to fit the screen correctly
